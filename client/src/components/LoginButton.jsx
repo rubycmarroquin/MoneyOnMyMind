b04ef65fbce011f5b87b78b0e3f8a0b6130be99e
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-01 12:08:08 -0700

    fixed (logoutbutton.jsx): updated to include redirect

--- client/src/components/LoginButton.jsx
+++ client/src/components/LoginButton.jsx
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => logininpop()}>Log In</button>;
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
