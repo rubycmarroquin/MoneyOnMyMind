c02e69d0c1d6fd23477467670e068fa281cd873b
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-02 16:40:35 -0700

    removed auth0 code from main file and added to new file

---
+++ client/src/auth/Auth0WithNavigate.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
+const Auth0WithNavigate = ({ children }) => {
+  const navigate = useNavigate();
+
+  const onRedirectCallback = (appState) => {
+    navigate(appState?.returnTo || window.location.pathname);
+  };
+
+  return (
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      useRefreshTokens
+      cacheLocation="localstorage"
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0WithNavigate;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Auth0WithNavigate from "./auth/Auth0WithNavigate";
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider
-    domain={import.meta.env.VITE_AUTH0_DOMAIN}
-    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-    useRefreshTokens
-    cacheLocation="localstorage"
-  >
-    <App />
-  </Auth0Provider>
+  <BrowserRouter>
+    <Auth0WithNavigate>
+      <App />
+    </Auth0WithNavigate>
+  </BrowserRouter>
 );
