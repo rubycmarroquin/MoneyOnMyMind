0654fab4ab9382564d01daabd45aed23a844117b
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-03 17:03:52 -0700

    added(ResetPassword.jsx): post method to send user email

--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -5,6 +5,26 @@ import { SnackbarContext } from "../components/SnackbarContext";
 const ResetButtonComp = ({ user_email }) => {
   const { open, handleOpen, handleClose } = useContext(SnackbarContext);
 
+  async function resetPassword() {
+    await fetch(
+      `https://${
+        import.meta.env.VITE_AUTH0_DOMAIN
+      }/dbconnections/change_password`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          client_id: import.meta.env.VITE_AUTH0_CLIENT_ID,
+          email: user_email,
+          connection: import.meta.env.VITE_DB_CONNECTION,
+        }),
+      }
+    ).then((response) => {
+      handleOpen("Email sent successfully!");
+      return response;
+    });
+  }
+
   return (
     <div id="ResetPasswordDiv">
       <h1>Change Password</h1>
