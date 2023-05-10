7bc5e362aef979ba99bd4546d50925d9b611b093
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-10 11:57:22 -0700

    changed(UserProfile.jsx): Updated to include context and not state for authtoken

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { AuthContext } from "./AuthContext";
 
