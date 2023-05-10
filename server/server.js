17368d87a57dde13b57ce7fb4d76719afc41ca01
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-10 07:50:58 -0700

    added(server.js): api endpoints to incomes table

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 /***************** Auth0 Connection **************/
 
 // create the get request for students in the endpoint '/api/students'
-app.get("/user/:userId", async (req, res) => {
+app.get("/user/:userId", cors(), async (req, res) => {
   const user_id = req.params.userId;
   try {
     const { rows: users } = await db.query(
@@ -44,7 +44,7 @@ app.get("/user/:userId", async (req, res) => {
 });
 
 // creates new entry for user, else does nothing
-app.post("/user", async (req, res) => {
+app.post("/user", cors(), async (req, res) => {
   console.log(req.body.user_id, req.body.email);
   try {
     const newUser = {
@@ -67,7 +67,7 @@ app.post("/user", async (req, res) => {
 /**************** Account Settings - Updating User Information ****************/
 
 // update a user's info
-app.put("/user/:userId", async (req, res) => {
+app.put("/user/:userId", cors(), async (req, res) => {
   const user_id = req.params.userId;
   const updatedUser = {
     name: req.body.name,
@@ -88,26 +88,26 @@ app.put("/user/:userId", async (req, res) => {
 });
 
 /*************** Expense Table METHODS *****************/
-// grab data by user id and month and year 
-app.get("/expenses/:userId&:monthName&:yearNum", async (req, res) => {
+// grab data by user id and month and year
+app.get("/expenses/:userId&:monthName&:yearNum", cors(), async (req, res) => {
   const user_id = req.params.userId;
   const monthName = req.params.monthName;
   const yearNum = req.params.yearNum;
   try {
-    const { rows: expenses } = await db.query(
-      "SELECT * FROM expenses WHERE user_id = $1 AND month iLIKE $2 AND year iLike $3",
+    const { rows: budgets } = await db.query(
+      `SELECT * FROM expenses WHERE user_id = $1 AND month iLIKE $2 AND year iLike $3`,
       [user_id, monthName, yearNum]
     );
-    console.log(expenses);
-    res.send(expenses);
+    console.log(budgets);
+    res.send(budgets);
   } catch (e) {
     return res.status(400).json({ e });
   }
 });
 
 // post expense to database
-app.post("/expenses", async (req, res) => {
-  const expense_id = randomUUID();
+app.post("/expenses", cors(), async (req, res) => {
+  const generated_id = randomUUID();
   try {
     const newExpense = {
       user_id: req.body.user_id,
@@ -116,7 +116,7 @@ app.post("/expenses", async (req, res) => {
       datepaid: req.body.datepaid || null,
       expense_name: req.body.expense_name,
       month: req.body.month,
-      year: req.body.year
+      year: req.body.year,
     };
 
     const result = await db.query(
@@ -129,7 +129,7 @@ app.post("/expenses", async (req, res) => {
         newExpense.expense_name,
         newExpense.month,
         newExpense.year,
-        expense_id,
+        generated_id,
       ]
     );
     res.json(result.rows[0]);
@@ -140,12 +140,10 @@ app.post("/expenses", async (req, res) => {
 });
 
 // delete an expense entry
-app.delete("/expense/:expenseId", async (req, res) => {
+app.delete("/expense/:expenseId", cors(), async (req, res) => {
   try {
     const expense_id = req.params.expenseId;
-    await db.query("DELETE FROM expenses WHERE expense_id=$1", [
-      expense_id,
-    ]);
+    await db.query("DELETE FROM expenses WHERE expense_id=$1", [expense_id]);
     console.log("From the delete request-url", expense_id);
     res.status(200).end();
   } catch (e) {
@@ -154,8 +152,8 @@ app.delete("/expense/:expenseId", async (req, res) => {
   }
 });
 
-// edit a user's expense 
-app.put("/expense/:expenseId", async (req, res) => {
+// edit a user's expense
+app.put("/expense/:expenseId", cors(), async (req, res) => {
   const expense_id = req.params.expenseId;
   try {
     const newExpense = {
@@ -172,7 +170,98 @@ app.put("/expense/:expenseId", async (req, res) => {
         newExpense.duedate,
         newExpense.datepaid,
         newExpense.expense_name,
-        expense_id
+        expense_id,
+      ]
+    );
+    res.json(result.rows[0]);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ e });
+  }
+});
+
+/*************** Income Table METHODS *****************/
+// grab income data by user id and month and year
+app.get("/incomes/:userId&:monthName&:yearNum", cors(), async (req, res) => {
+  const user_id = req.params.userId;
+  const monthName = req.params.monthName;
+  const yearNum = req.params.yearNum;
+  try {
+    const { rows: incomes } = await db.query(
+      "SELECT * FROM incomes WHERE user_id = $1 AND month iLIKE $2 AND year iLike $3",
+      [user_id, monthName, yearNum]
+    );
+    console.log(incomes);
+    res.send(incomes);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+});
+
+// post an income entry to database
+app.post("/incomes", cors(), async (req, res) => {
+  const income_id = randomUUID();
+  try {
+    const newIncome = {
+      amount: req.body.amount,
+      date: req.body.date || null,
+      user_id: req.body.user_id,
+      income_name: req.body.income_name,
+      month: req.body.month,
+      year: req.body.year
+    };
+
+    const result = await db.query(
+      "insert into incomes(user_id, amount, date, income_name, month, year, income_id) values($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [
+        newIncome.user_id,
+        newIncome.amount,
+        newIncome.date,
+        newIncome.income_name,
+        newIncome.month,
+        newIncome.year,
+        income_id,
+      ]
+    );
+    res.json(result.rows[0]);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ e });
+  }
+});
+
+// delete an income entry
+app.delete("/income/:incomeId", cors(), async (req, res) => {
+  try {
+    const income_id = req.params.incomeId;
+    await db.query("DELETE FROM incomes WHERE income_id=$1", [
+      income_id,
+    ]);
+    console.log("From the delete request-url", income_id);
+    res.status(200).end();
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ e });
+  }
+});
+
+// edit a user's income entry 
+app.put("/income/:incomeId", cors(), async (req, res) => {
+  const income_id = req.params.incomeId;
+  try {
+    const editedIncome = {
+      amount: req.body.amount,
+      date: req.body.date || null,
+      income_name: req.body.income_name,
+    };
+
+    const result = await db.query(
+      "update incomes set amount=$1, date=$2, income_name=$3 where income_id=$4 RETURNING *",
+      [
+        editedIncome.amount,
+        editedIncome.date,
+        editedIncome.income_name,
+        income_id
       ]
     );
     res.json(result.rows[0]);
