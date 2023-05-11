53857b5b0e9a1723d0d1851d3b992757a470d8a5
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-10 17:56:08 -0700

    created component for tags drop down for expenses

--- client/src/components/YearMonthDropDown.jsx
+++ client/src/components/YearMonthDropDown.jsx
@@ -10,31 +10,32 @@ const MonthDropDown = ({ month, setMonth, year, setYear }) => {
   ).sort((a, b) => a - b);
 
   return (
-    <div>
-      <h2>Select Month</h2>
-      <Dropdown
-        variant="secondary"
-        id="dropdown-basic"
-        onSelect={(e) => setMonth(e)}
-      >
-        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-          {month ? month : "Select Month"}
-        </Dropdown.Toggle>
-        <Dropdown.Menu style={{ overflowY: "scroll", height: "200px" }}>
-          <Dropdown.Item eventKey="January">January</Dropdown.Item>
-          <Dropdown.Item eventKey="Feburary">February</Dropdown.Item>
-          <Dropdown.Item eventKey="March">March</Dropdown.Item>
-          <Dropdown.Item eventKey="April">April</Dropdown.Item>
-          <Dropdown.Item eventKey="May">May</Dropdown.Item>
-          <Dropdown.Item eventKey="June">June</Dropdown.Item>
-          <Dropdown.Item eventKey="July">July</Dropdown.Item>
-          <Dropdown.Item eventKey="August">August</Dropdown.Item>
-          <Dropdown.Item eventKey="September">September</Dropdown.Item>
-          <Dropdown.Item eventKey="October">October</Dropdown.Item>
-          <Dropdown.Item eventKey="November">November</Dropdown.Item>
-          <Dropdown.Item eventKey="December">December</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+    <div id="MonthYearDD">
+      <div>
+        <Dropdown
+          variant="secondary"
+          id="dropdown-basic"
+          onSelect={(e) => setMonth(e)}
+        >
+          <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+            {month ? month : "Select Month"}
+          </Dropdown.Toggle>
+          <Dropdown.Menu style={{ overflowY: "scroll", height: "200px" }}>
+            <Dropdown.Item eventKey="January">January</Dropdown.Item>
+            <Dropdown.Item eventKey="Feburary">February</Dropdown.Item>
+            <Dropdown.Item eventKey="March">March</Dropdown.Item>
+            <Dropdown.Item eventKey="April">April</Dropdown.Item>
+            <Dropdown.Item eventKey="May">May</Dropdown.Item>
+            <Dropdown.Item eventKey="June">June</Dropdown.Item>
+            <Dropdown.Item eventKey="July">July</Dropdown.Item>
+            <Dropdown.Item eventKey="August">August</Dropdown.Item>
+            <Dropdown.Item eventKey="September">September</Dropdown.Item>
+            <Dropdown.Item eventKey="October">October</Dropdown.Item>
+            <Dropdown.Item eventKey="November">November</Dropdown.Item>
+            <Dropdown.Item eventKey="December">December</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
       {month ? (
         <div>
           <Dropdown
