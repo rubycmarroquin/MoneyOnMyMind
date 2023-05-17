eddd1eed2d11d480270107a6ba055a7e6e79eb7c
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-17 14:31:15 -0700

    Created(server.js):  YouTube API call, added mock data

--- server/server.js
+++ server/server.js
@@ -134,7 +134,7 @@ app.post("/expenses", cors(), async (req, res) => {
         newExpense.month,
         newExpense.year,
         generated_id,
-        newExpense.tags
+        newExpense.tags,
       ]
     );
     res.json(result.rows[0]);
@@ -166,7 +166,7 @@ app.put("/expense/:expenseId", cors(), async (req, res) => {
       duedate: req.body.duedate || null,
       datepaid: req.body.datepaid || null,
       expense_name: req.body.expense_name,
-      tags: req.body.tags || "Other"
+      tags: req.body.tags || "Other",
     };
 
     const result = await db.query(
@@ -279,11 +279,10 @@ app.put("/income/:incomeId", cors(), async (req, res) => {
   }
 });
 
-
 /***************************************************************************************************
  **************************************** YEAR API CALLS  ******************************************
  ***************************************************************************************************/
-// grab income amount by year and user id 
+// grab income amount by year and user id
 app.get("/yearly/income/:userId&:yearNum", cors(), async (req, res) => {
   const user_id = req.params.userId;
   const yearNum = req.params.yearNum;
@@ -299,7 +298,7 @@ app.get("/yearly/income/:userId&:yearNum", cors(), async (req, res) => {
   }
 });
 
-// grab expenses amount + tags by year and user id 
+// grab expenses amount + tags by year and user id
 app.get("/yearly/expenses/:userId&:yearNum", cors(), async (req, res) => {
   const user_id = req.params.userId;
   const yearNum = req.params.yearNum;
@@ -315,8 +314,144 @@ app.get("/yearly/expenses/:userId&:yearNum", cors(), async (req, res) => {
   }
 });
 
+/***************************************************************************************************
+ **************************************** YOUTUBE API CALLS ****************************************
+ ***************************************************************************************************/
 
+app.get("/api/videos/:keyword", async (req, res) => {
+  const mockData = [
+    {
+      kind: "youtube#searchResult",
+      etag: "RKQchR6oAHMwTUoZ5md4cSesJ54",
+      id: {
+        kind: "youtube#video",
+        videoId: "DfVB73q8EMY",
+      },
+      snippet: {
+        publishedAt: "2021-12-28T14:00:06Z",
+        channelId: "UC7eBNeDW1GQf2NJQ6G6gAxw",
+        title: "How Do I Tackle My $13,000 Credit Card Debt?",
+        description:
+          "How Do I Tackle My $13000 Credit Card Debt? Nix the guesswork and scrolling. We'll connect you with investment pros we trust: ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/DfVB73q8EMY/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/DfVB73q8EMY/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/DfVB73q8EMY/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "The Ramsey Show - Highlights",
+        liveBroadcastContent: "none",
+        publishTime: "2021-12-28T14:00:06Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "L4iWPdBNxco7uc3ns_BGy-pTJ1k",
+      id: {
+        kind: "youtube#video",
+        videoId: "mllmoeq2HYA",
+      },
+      snippet: {
+        publishedAt: "2018-08-17T21:00:09Z",
+        channelId: "UC7eBNeDW1GQf2NJQ6G6gAxw",
+        title: "How Do I Start Tackling My Debt?",
+        description:
+          "How Do I Start Tackling My Debt? Get a FREE customized plan for your money. It only takes 3 minutes! http://bit.ly/2YTMuQM Visit ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/mllmoeq2HYA/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/mllmoeq2HYA/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/mllmoeq2HYA/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "The Ramsey Show - Highlights",
+        liveBroadcastContent: "none",
+        publishTime: "2018-08-17T21:00:09Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "0HR2Q_h2_ZeHPBjopKW53KenjUc",
+      id: {
+        kind: "youtube#video",
+        videoId: "77U-782rsKw",
+      },
+      snippet: {
+        publishedAt: "2023-02-04T03:24:44Z",
+        channelId: "UCKjU3KzdbJE1EFcHVqXC3_g",
+        title: "Tackling debt as the cost of living rises | Quick Question",
+        description:
+          "Canadians are struggling with their finances amid high interest rates and inflation. Certified financial planner Zainab Williams and ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/77U-782rsKw/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/77U-782rsKw/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/77U-782rsKw/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "CBC News: The National",
+        liveBroadcastContent: "none",
+        publishTime: "2023-02-04T03:24:44Z",
+      },
+    },
+  ];
+  const maxResults = 3;
 
+  // const params = new URLSearchParams({
+  //   part: "snippet",
+  //   maxResults,
+  //   q: req.params.keyword,
+  //   key: process.env.YOUTUBE_API_KEY,
+  // });
+
+  // const url = `https://www.googleapis.com/youtube/v3/search?${params}`;
+  // console.log(url);
+
+  // // Make an API call to the YouTube API to get the latest videos
+  // await fetch(url)
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //     res.send(data.items);
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //     res.status(500).send("Error fetching videos");
+  //   });
+
+   res.send(mockData);
+});
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
