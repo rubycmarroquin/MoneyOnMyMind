64b09d1d03292f3e5488f91303040ea07d9c2e30
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-18 14:49:33 -0700

    added proxy configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,15 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      "/api":{
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   test: {
     framework: 'vitest',
     globals: true,
