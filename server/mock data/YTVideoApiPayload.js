9ce0da551cf5250bed5c86335947850011111264
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-22 15:25:28 -0700

    restructured: moved mock data for YouTube Videos into its own file

---
+++ server/mock data/YTVideoApiPayload.js
@@ -0,0 +1,107 @@
+module.exports = [
+  {
+    kind: "youtube#searchResult",
+    etag: "RKQchR6oAHMwTUoZ5md4cSesJ54",
+    id: {
+      kind: "youtube#video",
+      videoId: "DfVB73q8EMY",
+    },
+    snippet: {
+      publishedAt: "2021-12-28T14:00:06Z",
+      channelId: "UC7eBNeDW1GQf2NJQ6G6gAxw",
+      title: "How Do I Tackle My $13,000 Credit Card Debt?",
+      description:
+        "How Do I Tackle My $13000 Credit Card Debt? Nix the guesswork and scrolling. We'll connect you with investment pros we trust: ...",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/DfVB73q8EMY/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/DfVB73q8EMY/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/DfVB73q8EMY/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "The Ramsey Show - Highlights",
+      liveBroadcastContent: "none",
+      publishTime: "2021-12-28T14:00:06Z",
+    },
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "L4iWPdBNxco7uc3ns_BGy-pTJ1k",
+    id: {
+      kind: "youtube#video",
+      videoId: "mllmoeq2HYA",
+    },
+    snippet: {
+      publishedAt: "2018-08-17T21:00:09Z",
+      channelId: "UC7eBNeDW1GQf2NJQ6G6gAxw",
+      title: "How Do I Start Tackling My Debt?",
+      description:
+        "How Do I Start Tackling My Debt? Get a FREE customized plan for your money. It only takes 3 minutes! http://bit.ly/2YTMuQM Visit ...",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/mllmoeq2HYA/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/mllmoeq2HYA/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/mllmoeq2HYA/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "The Ramsey Show - Highlights",
+      liveBroadcastContent: "none",
+      publishTime: "2018-08-17T21:00:09Z",
+    },
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "0HR2Q_h2_ZeHPBjopKW53KenjUc",
+    id: {
+      kind: "youtube#video",
+      videoId: "77U-782rsKw",
+    },
+    snippet: {
+      publishedAt: "2023-02-04T03:24:44Z",
+      channelId: "UCKjU3KzdbJE1EFcHVqXC3_g",
+      title: "Tackling debt as the cost of living rises | Quick Question",
+      description:
+        "Canadians are struggling with their finances amid high interest rates and inflation. Certified financial planner Zainab Williams and ...",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/77U-782rsKw/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/77U-782rsKw/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/77U-782rsKw/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "CBC News: The National",
+      liveBroadcastContent: "none",
+      publishTime: "2023-02-04T03:24:44Z",
+    },
+  },
+];
