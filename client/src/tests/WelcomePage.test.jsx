9f21a559a95ff1e5a54861d061f9790c27148119
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-05-26 18:34:13 -0700

    Added 2 tests for WelcomePage

---
+++ client/src/tests/WelcomePage.test.jsx
@@ -0,0 +1,35 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import WelcomePage from "../pages/WelcomePage";
+import * as auth0 from "@auth0/auth0-react";
+
+vi.mock("@auth0/auth0-react");
+
+test("Welcome Page renders correctly", () => {
+  auth0.useAuth0 = vi.fn().mockReturnValue({
+    isAuthenticated: false,
+    user: { nickname: "Test" },
+  });
+  render(
+    <Router>
+      <WelcomePage />
+    </Router>
+  );
+  const welcomePageText = screen.getByText("Welcome");
+  expect(welcomePageText).toBeDefined();
+});
+
+test("WelcomePage when user is signed in", () => {
+  auth0.useAuth0 = vi.fn().mockReturnValue({
+    isAuthenticated: true,
+    user: { nickname: "Test" },
+  });
+  render(
+    <Router>
+      <WelcomePage />
+    </Router>
+  );
+  const welcomePageText = screen.getByText("Is Authenticated, redirecting...");
+  expect(welcomePageText).toBeDefined();
+});
